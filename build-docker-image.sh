a910aa2c44b6d563516942560b216d20844d68c9
iminders <liuwen.w@qq.com>
2020-03-20 20:08:56 +0800

    image

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,4 @@
 # 需要先在环境变量中设置 TUSHARE_TOKEN
-docker build --build-arg TUSHARE_TOKEN=$TUSHARE_TOKEN --build-arg BUILD_TIME=$(date +%s) . -t registry.cn-hangzhou.aliyuncs.com/aiminders/tenvs:latest
+docker build --build-arg TUSHARE_TOKEN=$TUSHARE_TOKEN --build-arg BUILD_TIME=$(date +%s) . -t tradingai/bazel:latest
 # 重新 build
-# docker build --no-cache --build-arg TUSHARE_TOKEN=$TUSHARE_TOKEN . -t registry.cn-hangzhou.aliyuncs.com/aiminders/tenvs:latest
+# docker build --no-cache --build-arg TUSHARE_TOKEN=$TUSHARE_TOKEN . -t tradingai/bazel:latest
